1400e6b9eab659fa758e998e197c370044d529e3
jrichardsz <jrichardsz.java@gmail.com>
2022-02-13 00:52:36 -0500

    add middleware and servlet init

--- src/main/application/RestApplicationStarter.js
+++ src/main/application/RestApplicationStarter.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid');
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
@@ -6,6 +7,7 @@ var path = require('path');
 var fs = require('fs');
 const fsPromises = fs.promises;
 const ConfigurationHelper = require('../configuration/ConfigurationHelper.js');
+const ObjectHelper = require('../common/ObjectHelper.js');
 
 const MetaJsContextHelper = require('meta-js').MetaJsContextHelper;
 const NodeInternalModulesHook = require('meta-js').NodeInternalModulesHook;
@@ -27,7 +29,7 @@ function RestApplicationStarter(){
     var dependencies;
     var environment = process.env.NODE_ENV
     if(environment !== 'production'){
-      var headAnnotations = ["Config","Route"];
+      var headAnnotations = ["Config","Route", "Middleware", "ServerInitializer"];
       var internalAnnotations = ["Autowire","Get","Post","Put","Delete", "Configuration"];
       dependencies = DependencyHelper.getDependecies(applicationRootLocation, [".js"], ["src/main/Index.js", ".test.js"],headAnnotations, internalAnnotations);
       console.log(JSON.stringify(dependencies, null,4 ));
@@ -45,31 +47,34 @@ function RestApplicationStarter(){
     this.addSpecialDependencies(applicationRootLocation);
     this.loadStarters(path.join(applicationRootLocation,"node_modules"));
     this.performInjection(dependencies);
-    this.startServer();
+    await this.startServer(dependencies);
+    this.registerMiddlewares(dependencies);
     this.registerRoutesMethods(dependencies);
   }
 
   this.performInstantation = (dependencies, applicationRootLocation) => {
-    console.log("Perform instantation...");
+    console.log("[Perform instantation]...");
     for(let dependency of dependencies){
       console.log("Detected dependency:"+dependency.meta.location);
-      if(this.instancedDependecies[dependency.meta.arguments.name]){
+      var instanceId = getInstanceId(dependency);
+      if(this.instancedDependecies[instanceId]){
         console.log("dependency is already instanced");
       }else{
         var absoluteModuleLocation = path.join(applicationRootLocation, dependency.meta.location);
         var functionRequire = require(absoluteModuleLocation);
         var functionInstance = new functionRequire();
-        this.instancedDependecies[dependency.meta.arguments.name] = functionInstance;
+        this.instancedDependecies[instanceId] = functionInstance;
       }
     }
   }
 
   this.performInjection = (dependencies, applicationRootLocation) => {
 
-    console.log("Perform autowire injection...");
+    console.log("[Perform autowire injection]...");
     for(let dependency of dependencies){
-      var functionInstance = this.instancedDependecies[dependency.meta.arguments.name];
-      console.log("Dependency:"+dependency.meta.arguments.name);
+      var instanceId = getInstanceId(dependency);
+      var functionInstance = this.instancedDependecies[instanceId];
+      console.log("Dependency:"+instanceId);
 
       if(Object.keys(dependency.variables).length > 0 && dependency.variables.constructor === Object){
         Object.keys(dependency.variables).forEach((variableToInject,index)=> {
@@ -84,6 +89,21 @@ function RestApplicationStarter(){
     }
   }
 
+  this.configureSession = () => {
+    var sessionConfig;
+    try{
+      sessionConfig = this.instancedDependecies["configuration"].nodeboot.session
+    }catch(err){
+      console.log("nodeboot.session is not well configured");
+      return;
+    }
+    console.log("Configuring session...");
+    var clonedConfiguration = ObjectHelper.clone(sessionConfig);
+    clonedConfiguration.secret = uuid.v4();
+    const session = require('express-session');
+    this.express.use(session(clonedConfiguration));
+  }
+
   this.addSpecialDependencies = (applicationRootLocation) => {
     //add custom modules to dependency context
     this.instancedDependecies["express"] = express || {};
@@ -92,24 +112,67 @@ function RestApplicationStarter(){
     var configurationHelper = new ConfigurationHelper();
     var configuration = configurationHelper.loadJsonFile(path.join(applicationRootLocation,"src","main","application.json"),'utf8');
     this.instancedDependecies["configuration"] = configuration || {};
+    this.instancedDependecies["rootPath"] = applicationRootLocation;
   }
 
-  this.startServer = () => {
-    this.express.use(bodyParser.urlencoded({extended: false}));
-    this.express.use(cors());
-    let port = process.env.PORT || 8080;
-    console.log("application is listening at port: "+port);
-    this.express.listen(port);
+  this.startServer = async (dependencies) => {
+    console.log("[Starting express server]...");
+    var instanceId;
+    for(let dependency of dependencies){
+      if(dependency.meta.name !== "ServerInitializer") continue;
+      if(typeof dependency.meta.arguments.name !== 'undefined'){
+        instanceId = dependency.meta.arguments.name;
+      }else{
+        instanceId = path.basename(dependency.meta.location).replace(".js","");
+      }
+      //just one ServerInitializer is allowed
+      break;
+    }
+    if(typeof instanceId === 'undefined'){
+      await startExpressServer();
+      return;
+    }
+
+    var serverInitializer = this.instancedDependecies[instanceId];
+    if(typeof serverInitializer.onBeforeLoad !== 'undefined'){
+      await serverInitializer.onBeforeLoad();
+      if(typeof serverInitializer.onAfterLoad !== 'undefined'){
+        await startExpressServer();
+        await serverInitializer.onAfterLoad()
+      }else{
+        await startExpressServer();
+      }
+    }
+  }
+
+  startExpressServer = () => {
+
+    return new Promise((resolve, reject) =>{
+      this.configureSession();
+      this.express.use(bodyParser.urlencoded({extended: false}));
+      this.express.use(cors());
+      let port = process.env.PORT || 8080;
+      if(typeof callback === 'undefined'){
+        console.log("application is listening at port: "+port);
+        this.express.listen(port);
+        resolve();
+      }else{
+        this.express.listen(port, () => {
+          console.log("application is listening at port: "+port);
+          resolve();
+        });
+      }
+    });
   }
 
   this.registerRoutesMethods = (dependencies) => {
-    console.log("Register routes ...");
+    console.log("[Registering routes]...");
     for(let dependency of dependencies){
       if(dependency.meta.name !== "Route") continue;
 
       var instanceId = dependency.meta.arguments.name;
       var globalRoutePath = dependency.meta.arguments.path;
-      console.log(`analizing ${instanceId} as ${globalRoutePath}`);
+      console.log(`candidate route: ${instanceId}`);
       //get annotated methods
       for(let functionName in dependency.functions){
         var annotations = dependency.functions[functionName];
@@ -120,16 +183,51 @@ function RestApplicationStarter(){
               console.log(`http method not allowed: ${method} in ${functionName}`);
               continue;
             }
-            this.express[method](`${globalRoutePath}${annotation.arguments.path}`,this.instancedDependecies[instanceId][functionName]);
-            console.log(`registered ${instanceId}.${functionName} as ${globalRoutePath}${annotation.arguments.path} method ${method}`);
+            var routeString;
+            if(typeof globalRoutePath !== 'undefined'){
+              if(typeof annotation.arguments.path !== 'undefined'){
+                routeString = `${globalRoutePath}${annotation.arguments.path}`;
+              }else{
+                routeString = `${globalRoutePath}`;
+              }
+            }else{
+              if(typeof annotation.arguments.path !== 'undefined'){
+                routeString = `${annotation.arguments.path}`;
+              }else{
+                console.log("route don't have path argument at module or method level. ");
+                continue;
+              }
+            }
+
+            this.express[method](routeString,this.instancedDependecies[instanceId][functionName]);
+            console.log(`registered route: ${instanceId}.${functionName} endpoint:${routeString} method:${method}`);
           }
         }
       }
     }
   }
 
+  this.registerMiddlewares = (dependencies) => {
+    console.log("[Register middlewares]...");
+    for(let dependency of dependencies){
+      if(dependency.meta.name !== "Middleware") continue;
+      let instanceId;
+      if(typeof dependency.meta.arguments.name !== 'undefined'){
+        instanceId = dependency.meta.arguments.name;
+      }else{
+        instanceId = path.basename(dependency.meta.location).replace(".js","");
+      }
+
+      if(typeof this.instancedDependecies[instanceId]['dispatch'] === 'undefined'){
+        console.log(`Middleware ${instanceId} don't have a dispatch function. Register is skipped.`);
+        continue;
+      }
+      this.express.use(this.instancedDependecies[instanceId]['dispatch']);
+    }
+  }
+
   this.loadStarters = (rootNodeModulesLocation) => {
-    console.log("Searching starters ...");
+    console.log("[Searching starters]...");
     try{
       if (require.resolve(rootNodeModulesLocation+'/nodeboot-database-starter')) {
          console.log("nodeboot-database-starter was detected. Configuring...");
@@ -146,6 +244,13 @@ function RestApplicationStarter(){
       console.log(err);
       if(err.code!="MODULE_NOT_FOUND"){
       }
+    }
+  }
+  getInstanceId = (dependency) => {
+    if(typeof dependency.meta.arguments.name !== 'undefined'){
+      return dependency.meta.arguments.name;
+    }else{
+      return path.basename(dependency.meta.location).replace(".js","");
     }
   }
 }
